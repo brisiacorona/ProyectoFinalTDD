df06a21dd52a74486d222ccb2931c26f5d749aca
brisiacorona <brisiacorona@gmail.com>
2014-10-09 17:10:13 -0500

    AddFuctionDBTemplate2

--- sched/app.py
+++ sched/app.py
@@ -19,7 +19,7 @@ db.Model = Base
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('appointment/index.html')
 
 
 @app.route('/appointments/')
@@ -66,7 +66,16 @@ def appointment_edit(appointment_id):
 @app.route(
     '/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
 def appointment_delete(appointment_id):
-    raise NotImplementedError('DELETE')
+    """Delete record using HTTP DELETE, respond with JSON."""
+    appt = db.session.query(Appointment).get(appointment_id)
+    if appt is None:
+        # Abort with Not Found, but with simple JSON response.
+        response = jsonify({'status': 'Not Found'})
+        response.status = 404
+        return response
+    db.session.delete(appt)
+    db.session.commit()
+    return jsonify({'status': 'OK'})
 
 
 @app.route('/appointments/create/', methods=['GET', 'POST'])
