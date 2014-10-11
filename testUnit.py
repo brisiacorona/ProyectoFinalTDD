804bfc2c9f189411733bcdcdd56623cb4d4061d3
brisiacorona <brisiacorona@gmail.com>
2014-10-11 11:12:13 -0500

    unittest1

--- testUnit.py
+++ testUnit.py
@@ -1,26 +1,31 @@
 import unittest
-import sched.app as app
+from sched import forms
+from sched.app import app
 
-class app(unittest.TestCase):
-	def setUp(self):
-		self.appt = app.app.user()
+class Form(unittest.TestCase):
+	def testform(self):
+		form = forms.AppointmentForm()
+		self.assertEqual(
+			'<input id="title" name="title" type="text" value="">', str(form.title))
+		self.assertEqual(
+			'<input id="start" name="start" type="text" value="">', str(form.start))
+		self.assertEqual(
+			'<input id="end" name="end" type="text" value="">', str(form.end))
+		self.assertEqual(
+			'<input id="allday" name="allday" type="checkbox" value="y">', str(form.allday))
+		self.assertEqual(
+			'<input id="location" name="location" type="text" value="">', str(form.location))
+		self.assertEqual(
+			'<textarea id="description" name="description"></textarea>', str(form.description))
+	
+	def testLogin(self):
+		form = forms.LoginForm()
+		self.assertEqual(
+			'<input id="username" name="username" type="text" value="">', str(form.username))
+		self.assertEqual(
+			'<input id="password" name="password" type="password" value="">', str(form.password))
 
-	def appointment_list(self):
-		response = self.appointment_list("/appointments")
-		self.assertEquals(response.status_code)
-		assert 'Redirecting' in response.data
 
-#se encuentra un evento establecido
-	def appointment_detail(self):
-		response = self.appt.post('/login/', data = dict(username = 'sun.prinsses@hotmail.com', password = 'mayra'), follow_redirects = true)
-		response = self.appt.get('/appointments/1/')
-		self.assertEquals(response.status_code)
-		assert "titulo" in response.data
-		assert "cita" not in response.data
-
-	def appointment_detail(self):
-		response = self.appt.post('/login/', data = dict(username = 'sun.prinsses@hotmail.com', password = 'mayra'), follow_redirects = true)
-		response = self.appt.get('/appointments/0/')
-		self.assertEquals(response.status_code)
-		assert "Not Found" in response.data
+if __name__ == '__main__':
+	unittest.main()
 
