d96387f4507a03210eccc65a0882d57a9d81c491
LUNAMAYRA <moon.prinsses@hotmail.com>
2014-10-13 00:46:15 -0500

    Cambios en feature

---
+++ tests/features/scenario.feature
@@ -0,0 +1,47 @@
+Feature: Do appointments
+
+Scenario: Open the page
+	Given: Enter the URL "http://127.0.0.1:5000/appointments/"
+	When: Data entry field "username" with "moon.prinsses@hotmail.com"
+	And: Data entry field "password" with "mayra"
+	And: Click the button submit
+
+Scenario: Create new appoitment
+	Given: Enter the URL "http://127.0.0.1:5000/appointments/create/"
+	When: Data entry field "title" with "Cita con el abogado"
+	And: Data entry field "start" with "2010-11-11 12:00:00"
+	And: Data entry field "end" with "2010-11-11 13:00:00"
+	And: Data entry field "location" with "Rio Grande"
+	And: Data entry field "description" with "Notariar el titulo"
+	And: Click the button submit
+
+Scenario: Consult appoitment
+	Given: Enter the URL "http://127.0.0.1:5000/appointments/1/"
+	Then: You can see "appointment-detail" contains "limpieza dental"
+
+Scenario: If the appointments dont exist
+	Given: Enter the URL "http://127.0.0.1:5000/appointments/0/"
+	Then: You can see error "Not Found"
+
+Scenario: Edit a given appointment
+	Given: Enter the URL "http://127.0.0.1:5000/appointments/1/edit"
+	When: You update the field "title" with "Titulo Nuevo"
+	And: Click the button submit
+	Then: You can see that the element with class "appointment-detail" contains "Titulo Nuevo"
+
+Scenario: Edit a date given appointment
+	Given: Enter the URL "http://127.0.0.1:5000/appointments/1/edit"
+	When: You update the field "start" with actual date
+	And: Click the button submit
+	Then: You can see that the element with class "appointment-detail" contains the actual date
+
+Scenario: List all appoitments
+	Given: Enter the URL "http://127.0.0.1:5000/appointments/"
+	Then I see at least "2" appoitments with the class "appointment-detail"
+
+Scenario: Delete appoitment
+	Given: Enter the URL "http://127.0.0.1:5000/appointments/"
+	When: You can select the appointment with the title "Cita con el abogado"
+	And: You can do click in the button "appointment-delete-link"
+	And: Enter the URL "http://127.0.0.1:5000/appointments/"
+	Then: You can see that the element with the class "appointment-detail" not contains "Cita con el abogado"
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -22,96 +22,112 @@ def find_field_by_class(browser, attribute):
     return elems[0] if elems else False
 
 
-@step('I go to "([^"]*)"')
-def given_i_go_to_url(step, url):
+@step(u'Given: Enter the URL "([^"]*)"')
+def given_enter_the_url_group1(step, url):
     world.response = world.browser.get(url)
 
 
-@step('I fill in field with id "([^"]*)" with "([^"]*)"')
-def when_i_fill_in_field_with_id_group1_with_group2(step, field_id, value):
+@step(u'When: Data entry field "([^"]*)" with "([^"]*)"')
+def when_data_entry_field_group1_with_group2(step, field_id, value):
     with AssertContextManager(step):
         text_field = world.browser.find_element_by_id(field_id)
         text_field.clear()
         text_field.send_keys(value)
 
 
-@step('I fill in field with id "([^"]*)" with "([^"]*)"')
-def and_i_fill_in_field_with_id_group1_with_group2(step, field_id, value):
+@step(u'And: Data entry field "([^"]*)" with "([^"]*)"')
+def and_data_entry_field_group1_with_group2(step, field_id, value):
     with AssertContextManager(step):
         text_field = world.browser.find_element_by_id(field_id)
         text_field.clear()
         text_field.send_keys(value)
 
 
-@step('I submit the form')
-def and_i_submit_the_form(step):
+@step(u'And: Click the button submit')
+def and_click_the_button_submit(step):
     with AssertContextManager(step):
         form = world.browser.find_element_by_class_name('form-horizontal')
         form.submit()
 
 
-@step('I do click in the button "([^"]*)"')
-def and_i_do_click_in_button(step, field_class):
-    with AssertContextManager(step):
-        button = world.browser.find_element_by_class_name(field_class)
-        button.click()
-
-
-@step('I see that the element with class "(.*?)" contains "(.*?)"')
+@step(u'Then: You can see "(.*?)" contains "(.*?)"')
 def element_contains(step, element_class, value):
     with AssertContextManager(step):
         element = world.browser.find_element_by_class_name(element_class)
         assert (value in element.text), "Got %s, %s " % (element.text, value)
 
 
-@step('I see that the title of the page contains "([^"]*)"')
-def then_i_see_the_title(step, title):
-    with AssertContextManager(step):
-        element = world.browser.find_element_by_tag_name('h2')
-        assert title == element.text, "Got %s " % element.text
+@step(u'Then: You can see error "([^"]*)"')
+def then_you_can_see_group1(step, title):
+	with AssertContextManager(step):
+		element = world.browser.find_element_by_tag_name('h2')
+		assert title == element.text, "Got %s " % element.text
 
 
-@step('I update the field with id "([^"]*)" with "([^"]*)"')
-def when_i_update(step, field_id, value):
+@step(u'When: You update the field "([^"]*)" with "([^"]*)"')
+def when_you_update_the_field_group1_with_group2(step, field_id, value):
     with AssertContextManager(step):
         text_field = world.browser.find_element_by_id(field_id)
-        text_field.clear()
-        text_field.send_keys(value)
+    text_field.clear()
+    text_field.send_keys(value)
+
 fechaActual = datetime.now().strftime("%Y-%m-%d %l:%M:%S")
 fechaActualComparacion = datetime.now().strftime("%Y-%m-%d")
 
 
-@step('I update the field with id "([^"]*)" with actual date')
-def when_i_update_with_actual_date(step, field_id):
+@step(u'Then: You can see that the element with class "([^"]*)" contains "([^"]*)"')
+def then_you_can_see_that_the_element_with_class_group1_contains_group2(step, element_class, title):
+    with AssertContextManager(step):
+        elements = world.browser.find_elements_by_class_name(element_class)
+        lst = []
+        for e in elements:
+            lst.append(e.text)
+        assert title not in lst
+
+
+@step(u'When: You update the field "([^"]*)" with actual date')
+def when_you_update_the_field_group1_with_actual_date(step, field_id):
     with AssertContextManager(step):
         text_field = world.browser.find_element_by_id(field_id)
         text_field.clear()
         text_field.send_keys(fechaActual)
 
 
-@step('I see that the element with class "([^"]*)" contains the actual date')
-def then_the_element_with_actual_date(step, element_class):
+@step(u'Then: You can see that the element with class "([^"]*)" contains the actual date')
+def then_you_can_see_that_the_element_with_class_group1_contains_the_actual_date(step, element_class):
     with AssertContextManager(step):
         element = world.browser.find_element_by_class_name(element_class)
         assert fechaActualComparacion in element.text, "Got %s " % element.text
 
 
-@step('I see at least "([^"]*)" appoitments with the class "([^"]*)"')
+@step(u'Then I see at least "([^"]*)" appoitments with the class "([^"]*)"')
 def then_i_see_two_appoitments(step, num, element_class):
     with AssertContextManager(step):
         elements = world.browser.find_elements_by_class_name(element_class)
         assert len(elements) > int(num)
 
 
-@step('I select the appointment with the title "([^"]*)"')
-def when_i_select_the_appointment_with_the_title(step, title):
+@step(u'When: You can select the appointment with the title "([^"]*)"')
+def when_you_can_select_the_appointment_with_the_title_group1(step, title):
     with AssertContextManager(step):
         element = world.browser.find_element_by_link_text(title)
         element.click()
 
 
-@step('I see that the element with the class "([^"]*)" not contains "([^"]*)"')
-def then_the_element_with_the_class_not_contains(step, element_class, title):
+@step(u'And: You can do click in the button "([^"]*)"')
+def and_you_can_do_click_in_the_button_group1(step, field_class):
+    with AssertContextManager(step):
+        button = world.browser.find_element_by_class_name(field_class)
+        button.click()
+
+
+@step(u'And: Enter the URL "([^"]*)"')
+def and_enter_the_url_group1(step, url):
+    world.response = world.browser.get(url)
+
+
+@step(u'Then: You can see that the element with the class "([^"]*)" not contains "([^"]*)"')
+def then_you_can_see_that_the_element_with_the_class_group1_not_contains_group2(step, element_class, title):
     with AssertContextManager(step):
         elements = world.browser.find_elements_by_class_name(element_class)
         lst = []
